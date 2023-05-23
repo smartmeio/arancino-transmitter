f5b7b10e9fbb6e6b34db5b18072b4d2298b95487
Sergio <sergio@smartme.io>
2023-05-23 11:53:09 +0200

    update SenderMqttS4T channel separator and docs

--- arancino_transmitter/transmitter/sender/SenderMqttS4T.py
+++ arancino_transmitter/transmitter/sender/SenderMqttS4T.py
@@ -34,19 +34,48 @@ class SenderMqttS4T(SenderMqtt):
         super().__init__(cfg=cfg)
         
     def _do_trasmission(self, data=None, metadata=None):
+        '''
+
+        Implementazione del metodo do_transmission della classe concreta SenderMqttS4T
+
+        :param data: contiene le metriche
+        :param metadata: contiene dati aggiuntivi per caratterizzare la metrica, solitamente
+            contiene: key, flow_name, tags, labels
+        :return:
+        '''
+
+        # tags sono una serie di dati passati nel topic che servono al nostro sistema cloud,
+        # con lo scopo di caratterizzare la metrica
         tags = ""
+
+
+        # estrae dai metadata le labels e le inserisce come tags
+        # solitamente labels contiene le chiavi: device_id, port_id, port_type
         for key, value in metadata["labels"].items():
             tmp = "{}={}".format(key, value)
             tags += tmp + '/'
+
+        # estrae dai metadata i tags e li inserisce come tags
         for key, value in metadata["tags"].items():
             tmp = "{}={}".format(key, value)
             tags += tmp + '/'
-        full_field=metadata["key"].split(':')[1]
-        channel="0"
-        field=full_field
-        if '-'in full_field:
-            field=full_field.split('-')[0]
-            channel=full_field.split('-')[1]
+
+
+        # key rappresenta il nome della metrica ed è composta dalla
+        # coppia "port_id:key". Quindi tramite split prendo il secondo
+        # elemento alla posizione 1. Ciò che viene estratto a sua volta
+        # potrebbe contenere informazioni sulla sorgente/canale della metrica
+        # di conseguenza se presente il simbolo "-" viene eseguito ulteriore split
+        # se non presente aggiungo come tag della metrica la coppia "channel=0"
+
+        full_field = metadata["key"].split(':')[1]
+        field = full_field
+        channel = "0"
+
+        if '#' in full_field:
+            field, channel = full_field.split('#')
+
         tags += "channel={}/".format(channel)
         self._topic = "{}/{}{}".format(metadata["db_name"], tags, field)
+
         return super()._do_trasmission(data=data, metadata=metadata)
